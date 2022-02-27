808f06d7220e76b9ea6074e6192b585b35ba215e
ajmarrocco <ajmarrocco@gmail.com>
2022-02-27 16:19:28 -0500

    console logs rows

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+// query the database
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
